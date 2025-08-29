64b80e83e228ea224a3580f3f495023b554412a0
Matthew Tuson <mtuson1414@gmail.com>
2025-08-29 13:38:50 +1200

    installed angular material imported fonts and icon types.  Setup basic theming and mat button overrides file.  Built register page markup

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full'
+  }
+];
---
+++ ClientApp/src/app/pages/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-register',
+  imports: [MatIconModule, MatButtonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+
+}
