39c25e644ca1634dd267cd8c05791783c7883818
officialMattTuson <mtuson1414@gmail.com>
2025-09-13 01:05:25 +1200

    extend karma config file

--- ClientApp/karma.conf.js
+++ ClientApp/karma.conf.js
@@ -1,11 +1,35 @@
 module.exports = function (config) {
   config.set({
-    browsers: ['ChromeHeadless'],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: process.env.CI ? ['ChromeHeadlessCI'] : ['Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
       }
     },
+    singleRun: process.env.CI ? true : false,
+    restartOnFileChange: true
   });
 };
