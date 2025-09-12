7ef06db2a7385d768b32d0832bc5aed58cc790e7
officialMattTuson <mtuson1414@gmail.com>
2025-09-13 10:28:32 +1200

    create trip summary component

---
+++ ClientApp/src/app/pages/dashboard/components/trip-summary/trip-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-summary',
+  imports: [],
+  templateUrl: './trip-summary.component.html',
+  styleUrl: './trip-summary.component.scss'
+})
+export class TripSummaryComponent {
+
+}
